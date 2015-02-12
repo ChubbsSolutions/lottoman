f9207fed9464ce3f62508f10e866bd51f4e6462b
Chubbs' Analytics <lottoman@ip-172-31-5-92.ec2.internal>
2015-02-12 19:40:05 +0000

    Adding Python Scripts

---
+++ hoosierlotto.py
@@ -0,0 +1,6 @@
+import random 
+n = 1
+while n <= 6:
+  z = random.randint(1, 48)
+  print z
+  n += 1
---
+++ powerball.py
@@ -0,0 +1,8 @@
+import random 
+n = 1
+while n <= 5:
+  z = random.randint(1, 59)
+  print z
+  n += 1
+w = random.randint(1, 35)
+print w, '- powerball' 
