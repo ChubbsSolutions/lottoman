db7d21ab00c596e975ed74c57355adddfcc6fdb8
Ismael Arenzana <ismael.arenzana@trnswrks.com>
2015-02-13 09:57:39 -0500

    Rewrote the app to add powerball. Fixed README to add more information.

--- README.md
+++ README.md
@@ -19,3 +19,27 @@ cd lottoman
 go get https://github.com/mitchellh/gox
 go run 6numbers.go
 ```
+
+##Usage
+
+```NAME:
+   lottoman - Application to get the lucky numbers for the lotto.
+
+USAGE:
+   lottoman [global options] command [command options] [arguments...]
+
+VERSION:
+   0.3
+
+AUTHOR:
+  Chubbs Solutions - <lottoman@chubbs.solutions>
+
+COMMANDS:
+   hoosierlotto, hl	Get numbers for the Hoosier Lotto
+   powerball, pb Get numbers for the powerball
+   help, h    	 Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h		show help
+   --version, -v	print the version
+```
---
+++ lottoman.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+    "github.com/codegangsta/cli"
+    "os"
+)
+
+//Define constants
+const version string = "0.3"
+const author string = "Chubbs Solutions"
+const email string = "lottoman@chubbs.solutions"
+const appName string = "lottoman"
+const appDescription string = "Application to get the lucky numbers for the lotto."
+
+//Evaluate options on main
+func main() {
+
+app := cli.NewApp()
+app.Name = appName
+app.Usage = appDescription
+app.Email = email
+app.Author = author
+app.Version = version
+app.Commands = []cli.Command{
+    {
+        Name:      "hoosierlotto",
+        ShortName: "hl",
+        Usage:     "Get numbers for the Hoosier Lotto",
+        Action: func(c *cli.Context) {
+            
+            //Get random numbers
+
+            i := 0
+            var num []int
+            for i < 6 {
+                rand.Seed(time.Now().UnixNano())
+                temp:=rand.Intn(35)
+                if IsNumGood(num,temp) {
+                     fmt.Print(temp,"\n")
+                     num=append(num,temp)
+                     i++
+                }
+            }
+        },
+    },
+    {
+        Name:      "powerball",
+        ShortName: "pb",
+        Usage:     "Get numbers for the powerball",
+        Action: func(c *cli.Context) {
+            i := 0
+            var num []int
+            for i < 5 {
+                rand.Seed(time.Now().UnixNano())
+                temp:=rand.Intn(59)
+                if IsNumGood(num,temp) {
+                     fmt.Print(temp,"\n")
+                     num=append(num,temp)
+                     i++
+                }
+            }
+            x := 0
+            for x == 0 {
+                rand.Seed(time.Now().UnixNano())
+                pb := rand.Intn(35)
+                if pb != 0 {
+                    fmt.Println("\nPowerball: ",pb)
+                    x = 1;
+                }                
+            }
+        },
+    },
+}
+
+app.Run(os.Args)
+
+}
+//Function to make sure the number is unique.
+func IsNumGood(slice []int, proposed int) bool {
+    for _, ele := range slice {
+        if ele == proposed || proposed == 0 {
+            return false
+        }
+    }
+    return true
+}
