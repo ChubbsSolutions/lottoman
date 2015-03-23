ae65218f3e9f051bb59f1f2f5a08d153355cc6a3
Ismael Arenzana <iarenzana@gmail.com>
2015-03-23 13:17:43 -0400

    Test Unit set up.

--- lottoman.go
+++ lottoman.go
@@ -30,20 +30,7 @@ app.Commands = []cli.Command{
         ShortName: "hl",
         Usage:     "Get numbers for the Hoosier Lotto",
         Action: func(c *cli.Context) {
-            
-            //Get random numbers
-
-            i := 0
-            var num []int
-            for i < 6 {
-                rand.Seed(time.Now().UnixNano())
-                temp:=rand.Intn(35)
-                if IsNumGood(num,temp) {
-                     fmt.Print(temp,"\n")
-                     num=append(num,temp)
-                     i++
-                }
-            }
+            hoosierLottoGet()
         },
     },
     {
@@ -51,26 +38,7 @@ app.Commands = []cli.Command{
         ShortName: "pb",
         Usage:     "Get numbers for the powerball",
         Action: func(c *cli.Context) {
-            i := 0
-            var num []int
-            for i < 5 {
-                rand.Seed(time.Now().UnixNano())
-                temp:=rand.Intn(59)
-                if IsNumGood(num,temp) {
-                     fmt.Print(temp,"\n")
-                     num=append(num,temp)
-                     i++
-                }
-            }
-            x := 0
-            for x == 0 {
-                rand.Seed(time.Now().UnixNano())
-                pb := rand.Intn(35)
-                if pb != 0 {
-                    fmt.Println("\nPowerball: ",pb)
-                    x = 1;
-                }                
-            }
+            powerballGet()
         },
     },
 }
@@ -78,12 +46,53 @@ app.Commands = []cli.Command{
 app.Run(os.Args)
 
 }
-//Function to make sure the number is unique.
+
+//IsNumGood Function to make sure the number is unique.
 func IsNumGood(slice []int, proposed int) bool {
     for _, ele := range slice {
         if ele == proposed || proposed == 0 {
             return false
         }
+    }
+    return true
+}
+
+//hoosierLottoGet Get numbers for the Hoosier Lotto
+func hoosierLottoGet() bool {
+    i := 0
+    var num []int
+    for i < 6 {
+        rand.Seed(time.Now().UnixNano())
+        temp:=rand.Intn(35)
+        if IsNumGood(num,temp) {
+            fmt.Print(temp,"\n")
+            num=append(num,temp)
+            i++
+        }
+    }
+    return true
+}
+
+func powerballGet() bool {
+    i := 0
+    var num []int
+    for i < 5 {
+        rand.Seed(time.Now().UnixNano())
+        temp:=rand.Intn(59)
+        if IsNumGood(num,temp) {
+             fmt.Print(temp,"\n")
+             num=append(num,temp)
+             i++
+        }
+    }
+    x := 0
+    for x == 0 {
+        rand.Seed(time.Now().UnixNano())
+        pb := rand.Intn(35)
+        if pb != 0 {
+            fmt.Println("\nPowerball: ",pb)
+            x = 1;
+        }                
     }
     return true
 }
